095b19a61088cb1e5d49872712b5ddc87cec3159
Anjali17aj <sayhellotoanjali@gmail.com>
2023-03-01 13:41:01 +0530

    175. Combine two tables

---
+++ LeetCodeSQL/175. 172Combine Two Tables.sql
@@ -0,0 +1,7 @@
+175. Combine Two Tables
+
+# Write your MySQL query statement below
+SELECT p.firstName, p.lastName, a.city, a.state  #sql command
+FROM Person p
+LEFT JOIN Address a
+ON p.personId = a.personID
