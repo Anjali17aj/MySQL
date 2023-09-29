44f9bf3be81eddb79feeda12b0f2bb54936bdfe6
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-09-29 10:11:50 +0530

    8. WHERE CLAUSE.

---
+++ WHERE CLAUSE.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE IF NOT EXISTS instagram;
+SHOW DATABASES;
+USE instagram;
+
+CREATE TABLE user (
+id INT,
+age INT,
+name VARCHAR(30) NOT NULL,
+email VARCHAR(20) UNIQUE,
+followers INT DEFAULT 0,
+following INT,
+CONSTRAINT age_check CHECK (age>=13),
+PRIMARY KEY(id)
+);
+
+INSERT INTO user
+(id, age, name, email, followers, following)
+VALUES
+(1, 24, "ANJALI KUMARI", "sayhellotoanjali@gmail.com", 1500, 880),
+(2, 20, "ASHWINI KUMAR", "helloashwini@gmail.com", 450, 88),
+(3, 40, "ANITA RAM", "sayhellotoanita@gmail.com", 150, 20);
+
+
+SELECT * 
+FROM user
+WHERE followers >= 200;
+
+SELECT name, followers 
+FROM user
+WHERE followers >= 200;
+
