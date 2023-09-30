30c3e1b36bada74324becf19c8dfb46c809c4389
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-09-30 11:24:58 +0530

    28. UNION

---
+++ UNION.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE Employee;
+
+USE Employee;
+
+CREATE TABLE emp(
+id INT PRIMARY KEY,
+name VARCHAR(50),
+manager_id INT
+);
+
+INSERT INTO emp (id, name, manager_id)
+VALUES
+(101, "ANJALI", 103),
+(102, "ASHWINI", 104),
+(103, "PRACHI", NULL),
+(104, "CHIKY", 103);
+
+INSERT INTO emp (id, name, manager_id)
+VALUES
+(105, "ANITA", 109),
+(106, "ASH", 103);
+
+SELECT * FROM emp as e;
+
+SELECT name FROM emp
+UNION 
+SELECT name FROM emp;
+
+SELECT name FROM emp
+UNION ALL
+SELECT name FROM emp;
